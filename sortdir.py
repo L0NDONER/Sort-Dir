c2c3e7452dc1e9554f9ee1abe958374a17296e8e
L0NDONER <oflaherty@gmail.com>
2017-01-16 14:07:25 +0000

    Version 2 of sort directory

--- sortdir.py
+++ sortdir.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 import os
 import shutil
 import time
 
 # The Path of the directory to be sorted
 
-path = '/home/l0ndoner/Downloads'
+path = 'path/to/directory/you/want/sorted'
 
 
 # This populates a list with the filenames in the directory
@@ -15,7 +18,7 @@ print(time.strftime('%a - %H:%M:%S - %Y'))
 
 # Traverses every file
 for file_ in list_:
-    name,ext = os.path.splitext(file_)
+    name, ext = os.path.splitext(file_)
     # Stores the extension type
     ext = ext[1:]
     # If it is directory, it forces the next iteration
@@ -23,9 +26,9 @@ for file_ in list_:
         continue
     # It moves the file to that directory
     if os.path.exists(path+'/'+ext):
-        shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
+        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
         # If the directory does not exist, it creates a new directory
     else:
         os.makedirs(path+'/'+ext)
-        shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
+shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
 
